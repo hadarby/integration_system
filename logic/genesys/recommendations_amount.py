a2720fabda25b5416786daf82d912c8b50a7462a
hadarby <hadarbey@gmail.com>
2021-01-07 11:02:40 +0200

    'update'

--- logic/genesys/recommendations_amount.py
+++ logic/genesys/recommendations_amount.py
@@ -1,25 +1,25 @@
-import json
-import logging
-
-import PureCloudPlatformClientV2
-import websockets
-from PureCloudPlatformClientV2.rest import ApiException
-
 from logic.recommendations_amount import RecommendationsAmount
+import logging
+import asyncio
+import json
+import websockets
+import PureCloudPlatformClientV2
+from PureCloudPlatformClientV2.rest import ApiException
 
 
 class RecommendationsAmountGenesys(RecommendationsAmount):
-    def __init__(self, recommendations_amount_topic, ratio=2, buffer=0):
-        super(RecommendationsAmountGenesys, self).__init__(recommendations_amount_topic, ratio, buffer)
+    def __init__(self, rat=2, buf=0):
+        super(RecommendationsAmountGenesys, self).__init__(rat, buf)
         # Authenticate with genesys cloud
-        api_client = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token('', '')
+        api_client = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(
+            '8f54c539-eb9b-4fbe-9d8a-8ec8789cd2b5', 'cLGhZPGfU7JMNXudSlVQW364es89bNimFttc7Ymr1WE')
         # Create an instance of the Routing API and Analytics API
         self._routing_api = PureCloudPlatformClientV2.RoutingApi(api_client)
         self._analytics_api = PureCloudPlatformClientV2.AnalyticsApi(api_client)
         # Create an instance of the API class
         self._api_instance = PureCloudPlatformClientV2.AnalyticsApi(api_client)
         self._notifications_api = PureCloudPlatformClientV2.NotificationsApi(api_client)
-        self._queue_id = ""
+        self._queue_id = "76f78497-becd-4ce1-b6f1-a9c26a8669ec"
         self._channel = self.create_channel()
         self._conversations_topic_id = self.subscribe_to_conversation()
 
@@ -29,12 +29,12 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
         :return: new_channel
         """
         new_channel = None
-
         try:
             new_channel = self._notifications_api.post_notifications_channels()
             logging.info("Created a channel")
         except ApiException as e:
             logging.error("Exception when calling NotificationsApi->post_notifications_channels: {}".format(e))
+            # sys.exit(response.status_code)
         return new_channel
 
     def subscribe_to_conversation(self):
@@ -50,11 +50,12 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
             notification_subscription = self._notifications_api. \
                 put_notifications_channel_subscriptions(self._channel.id, [channel_topic])
             logging.info(notification_subscription)
-        except ApiException as ex:
-            logging.error("Exception when calling NotificationsApi->put_notifications_channel_subscriptions: {}".format(ex))
+        except ApiException as e:
+            logging.error("Exception when calling NotificationsApi->put_notifications_channel_subscriptions: {}".format(e))
+            # sys.exit(response.status_code)
         return conversations_topic_id
 
-    async def start(self):
+    async def listen_to_websocket(self):
         """
         Open a new web socket using the connect Uri of the channel
         """
@@ -63,7 +64,6 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
             # Message received
             async for message in websocket:
                 message = json.loads(message)
-
                 if message['topicName'].lower() == "channel.metadata":
                     continue
                 if message['topicName'].lower() != self._conversations_topic_id:
@@ -72,7 +72,6 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
                     # filter each incoming interactions
                     purpose = ([x for x in message['eventBody']['participants'] if x['purpose'] == 'customer'])[0][
                         'purpose']
-
                     if purpose == 'customer':
                         self.display_queue_observation()
 
@@ -81,6 +80,7 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
         Query for queue observations: "oUserRoutingStatuses", "oWaiting".
         Send the results to self.send_recommendations_amount()
         """
+        query = PureCloudPlatformClientV2.QueueObservationQuery()  # QueueObservationQuery | query
         query = {
             "filter": {
                 "type": "AND",
@@ -102,28 +102,29 @@ class RecommendationsAmountGenesys(RecommendationsAmount):
             api_response = self._api_instance.post_analytics_queues_observations_query(query)
             dial = None
             available_agents = None
-
             for result in api_response.results:
                 if result.group.get('mediaType') == 'callback':
                     dial = result.data[0].stats.count
                     logging.info("There are {} leads in DIAL mode".format(dial))
-
                     continue
-
                 if result.group.get('mediaType', True):
                     for status in result.data:
                         if status.qualifier == 'IDLE':
                             available_agents = status.stats.count
                             logging.info("There are {} available agents".format(available_agents))
-
                             break
-
                 if dial is not None and available_agents is not None:
                     break
-
             if dial is not None and available_agents is not None:
                 self.send_recommendations_amount({'available_agents': available_agents, 'dial': dial})
-        except ApiException as ex:
-            logging.error("Exception when calling AnalyticsApi->post_analytics_queues_observations_query: {}".format(ex))
+        except ApiException as e:
+            logging.error("Exception when calling AnalyticsApi->post_analytics_queues_observations_query: %s\n" % e)
 
+    def start(self):
+        """
+        Subscribe to Genesys conversations topic
+        async listening to the topic
+        """
+        grouped_async = asyncio.gather(self.listen_to_websocket())
+        asyncio.get_event_loop().run_until_complete(grouped_async)
 
--- run_files/genesys/recommendations_amount.py
+++ run_files/genesys/recommendations_amount.py
@@ -1,26 +1,25 @@
-import asyncio
-import logging
-
 from logic.genesys.recommendations_amount import RecommendationsAmountGenesys
-from utils.logging_utils import LoggingUtils
+import logging
+import sys
+import asyncio
 
 
 def main():
-    LoggingUtils.setup_logs()
-    recommendations_amount_topic = 'recommendations_topic'
-    recommendations_amount = RecommendationsAmountGenesys(recommendations_amount_topic)
-
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+    recommendations_amount = RecommendationsAmountGenesys()
     try:
-        grouped_async = asyncio.gather(recommendations_amount.start())
-        asyncio.get_event_loop().run_until_complete(grouped_async)
+        recommendations_amount.start()
     except SystemExit:
         logging.info("RecommendationsAmountGenesys instructed to stop.")
     except Exception as ex:
-        logging.exception("Exception in RecommendationsAmountGenesys! - {}".format(ex))
-
-    if recommendations_amount is not None:
-        recommendations_amount.terminate()
-
+        logging.exception("Error in RecommendationsAmountGenesys! - {}".format(ex))
+    recommendations_amount.terminate()
     logging.info("RecommendationsAmountGenesys has terminated.")
 
 
